23c1185fd132ac7f3fbb0ed56b60f7c820caab56
fushengjun <fushengjun@xiangshang360.com>
2016-01-14 15:48:04 +0800

    Initial Commit

---
+++ SJSelectProAndCityDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SJSelectProAndCityDemo
+//
+//  Created by SPIREJ on 16/1/14.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SJSelectProAndCityDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SJSelectProAndCityDemo
+//
+//  Created by SPIREJ on 16/1/14.
+//  Copyright © 2016年 SPIREJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
